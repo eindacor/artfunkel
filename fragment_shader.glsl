7c8887ee215436f32cea1409076aebe4e83e2287
Joseph Pollack <jpollack320@gmail.com>
2015-05-07 09:41:13 -0400

    updated exe

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -5,10 +5,10 @@ in vec2 UV;
 uniform sampler2D myTextureSampler;
 uniform float dim_factor = 1.0f;
 
-out vec3 output_color;
+out vec4 output_color;
 
 void main()
 {
 	vec3 undimmed_color = texture2D( myTextureSampler, UV).rgb;
-	output_color = vec3(undimmed_color.x * dim_factor, undimmed_color.y * dim_factor, undimmed_color.z * dim_factor);
+	output_color = vec4(undimmed_color.x * dim_factor, undimmed_color.y * dim_factor, undimmed_color.z * dim_factor, 1.0f);
 }
